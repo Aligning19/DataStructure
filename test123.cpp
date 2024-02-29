27b12d92c1efe65e8703dd18ccc444edb16710eb
Aligning19 <160582966+Aligning19@users.noreply.github.com>
2024-02-29 16:57:02 +0700

    Add files via upload

---
+++ test123.cpp
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+//	 contains
+//	char ends[] = "Controller";
+//	char in[] = "FelixController";
+//	int endlen = strlen(in) - 1;
+//	if (strlen(in) < strlen(ends)){
+//		printf("Gak masuk");
+//		return 0;
+//	}
+//	for (int i = strlen(ends) - 1; i >= 0 ; i--){
+//		if (in[endlen] != ends[i]){
+//			printf("Gak Masuk");
+//			return 0;
+//		}
+//		endlen--;
+//	}
+//	printf("masuk\n");
+	// startsWith
+	
+	char in[] = "Hallo Saya Lagi Belajar Di "
+	
+	
+	return 0;
+}
